b4fb2dd119561c9fa5870fddf96ba409b5f2f669
joseph0017 <66425797+joseph0017@users.noreply.github.com>
2023-03-24 09:18:15 -0700

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
-# creates a file in /tmp
+# creates a file in tmp directory
 file { '/tmp/school':
   ensure  => 'present',
   path    => '/tmp/school',
-  mode	  => '0744',
-  owner	  => 'www-data',
-  group	  => 'www-data',
-  content => 'I love Puppet'
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
-
