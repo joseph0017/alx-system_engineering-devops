39688446e96a47cd9c06cace6a60e15ba36329d9
joseph0017 <66425797+joseph0017@users.noreply.github.com>
2023-03-26 18:02:04 -0700

    fixed

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,11 @@
 # configuration to connect to a server without typing a password
 file_line { 'disable password login':
+  ensure => 'present',
   path => '/etc/ssh/ssh_config',
   line => 'PasswordAuthentication no',
 }
-file_line { 'add path to find the keys':
+file_line { 'add path':
+  ensure => 'present',
   path => '/etc/ssh/ssh_config',
   line => 'IdentityFile ~/.ssh/school',
 }
