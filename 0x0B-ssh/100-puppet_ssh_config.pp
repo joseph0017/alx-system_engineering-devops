a931f7a131fb0db7ce9f10f7ccae5cb68ff7a795
joseph0017 <66425797+joseph0017@users.noreply.github.com>
2023-03-26 17:59:26 -0700

    Create 100-puppet_ssh_config.pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+# configuration to connect to a server without typing a password
+file_line { 'disable password login':
+  ensure => 'present',
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
+}
+file_line { 'add path':
+  ensure => 'present',
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/holberton',
+}
