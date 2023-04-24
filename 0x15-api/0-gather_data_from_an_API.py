ae97e4cb541cc99034b8bb02011bc1b00da779fc
joseph0017 <josephogiku96@gmail.com>
2023-04-24 14:00:47 -0700

    API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -11,10 +11,10 @@ if __name__ == '__main__':
     url = 'https://jsonplaceholder.typicode.com/'
     # parameter for employee id
     param = argv[1]
-    employee_by_id = requests.get(url + f'users/{param}').json()
+    employee_by_id = requests.get(url +'users/{}'.format(param)).json()
     name = employee_by_id.get('name')
     payload = {"userId":  param}
-    employee_todo = requests.get(url + f'todos/', params=payload).json()
+    employee_todo = requests.get(url + 'todos/', params=payload).json()
 
     done_task = 0
     get_tasks_done = []
@@ -23,8 +23,9 @@ if __name__ == '__main__':
             get_tasks_done.append(task)
             done_task += 1
     num_tasks = len(employee_todo)
-    print(f"Employee {name} is done with tasks({done_task}/{num_tasks})")
+    print("Employee {} is done with tasks({}/{})"\
+          .format(name, done_task, num_tasks))
 
     for task in get_tasks_done:
         title = task.get('title')
-        print(f"\t {title}")
+        print("\t {}".format(title))
