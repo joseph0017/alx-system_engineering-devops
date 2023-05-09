46da7baaed71bb8deb61dc8a66822f7b8d1846e0
joseph0017 <josephogiku96@gmail.com>
2023-05-09 03:23:44 -0700

    advanced api

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Write a function that queries the Reddit API and
+prints the titles of the first 10 hot posts listed for a given subreddit.
+"""
+
+import requests
+
+def top_ten(subreddit):
+    """
+    returns the hot hot absolute hot top 10 redit posts
+    """
+    reddit_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    header = {"User-agent": "Joey"}
+    payload = {"limit": "10", "g": "GLOBAL"}
+    get_subreddit = requests.get(reddit_url, params = payload,
+                                 headers=header, allow_redirects=False)
+    if get_subreddit.status_code != 200:
+        return None
+    result = get_subreddit.json().get('data').get('children')
+    for content in result:
+        print(content.get('data').get('title'))
