cf9e38eab121de859b4c401a84a27c97189c8f39
joseph0017 <josephogiku96@gmail.com>
2023-05-09 23:39:02 -0700

    debug

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,8 @@
+# fixing 500 internal error in apache2
+# found in var/www/html wp-settings.php
+# on line: 137 require_once( ABSPATH . WPINC . '/class-wp-locale.phpp' );
+
+exec { 'var/www/html/wp-settings.php':
+  command => "sed -i 's/phpp/php/g' var/www/html/wp-settings.php",
+  path    => '/bin'
+}
