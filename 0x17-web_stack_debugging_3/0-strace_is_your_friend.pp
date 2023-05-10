3c58da0b0aedea865c3eb19ebef3a546feb61955
joseph0017 <josephogiku96@gmail.com>
2023-05-10 09:51:05 -0700

    dev

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,3 @@
-# fixing 500 internal error in apache2
-# found in var/www/html wp-settings.php
 # on line: 137 require_once( ABSPATH . WPINC . '/class-wp-locale.phpp' );
 
 exec { 'var/www/html/wp-settings.php':
