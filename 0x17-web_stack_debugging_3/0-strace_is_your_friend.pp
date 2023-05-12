b4ff3ac496fce0767acbaa242e24255294184887
joseph0017 <66425797+joseph0017@users.noreply.github.com>
2023-05-12 04:12:31 -0700

    strace

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,3 @@
 # fixing LAMP stack website and typos
 
-exec { 'fix website':
-  path    => '/usr/bin/env'
-  command => 'sed -i s/phpp/php/g var/www/html/wp-settings.php',
-}
+exec { '/usr/local/bin/:bin sed -i "s/phpp/php/g" var/www/html/wp-settings.php': }
