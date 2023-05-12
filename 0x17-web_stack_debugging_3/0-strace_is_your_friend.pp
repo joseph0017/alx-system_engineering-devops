5c94753e931814cc7ec26b10a78921b464ca264f
joseph0017 <66425797+joseph0017@users.noreply.github.com>
2023-05-12 03:49:48 -0700

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-# on line: 137 require_once( ABSPATH . WPINC . '/class-wp-locale.phpp' );
+
 
 exec { 'var/www/html/wp-settings.php':
   command => "sed -i 's/phpp/php/g' var/www/html/wp-settings.php",
